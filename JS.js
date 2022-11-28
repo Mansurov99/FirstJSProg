bdf7eab1f464b306e8a0b854799138021c90d8c6
Alnur <alnur2010@mail.ru>
2022-11-28 06:42:57 +0300

    Автоматизировали

--- JS.js
+++ JS.js
@@ -1,6 +1,7 @@
-// "use strict";
+"use strict";
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
+
 const personalMovieDB = {
    count: numberOfFilms,
    movies: {},
@@ -9,12 +10,26 @@ const personalMovieDB = {
    privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for(let i = 0; i < 2; i++){
+   const a = prompt('Один из последних просмотренных фильмов?', ''),
+         b = prompt('На сколько оцените его?', '');
+          
+         if(a != null && b != null && a != '' && b != '' && a.length < 50){
+           personalMovieDB.movies[a] = b;
+           console.log('done');
+         } else {
+           console.log('error');
+           i--;
+         }         
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if(personalMovieDB.count <= 10){
+   alert('Просмотрено довольно мало фильмов!');
+}else if(personalMovieDB.count <= 30){
+   alert('Вы классический зритель!');
+}else if(personalMovieDB.count > 30){
+   alert('Вы киноман!');
+}else{
+   alert('Произошла ошибка');
+}
 
-console.log(personalMovieDB);
